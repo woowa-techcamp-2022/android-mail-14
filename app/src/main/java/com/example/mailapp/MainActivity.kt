11bd845ff4739d034bd3444436d655a851ba7166
wjchoi96 <wjchoi96@naver.com>
2022-07-06 20:29:22 +0900

    [feat] navigation view 메뉴를 main activity 에 추가
     - 각 메뉴 클릭에는 임시 기능을 붙여놓은 상태

--- app/src/main/java/com/example/mailapp/MainActivity.kt
+++ app/src/main/java/com/example/mailapp/MainActivity.kt
@@ -4,7 +4,10 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.MenuItem
+import androidx.core.view.GravityCompat
 import androidx.lifecycle.ViewModelProvider
 import com.example.mailapp.databinding.ActivityMainBinding
 import com.example.mailapp.view.BaseActivity
@@ -34,7 +37,44 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
     }
 
     override fun initView(savedInstanceState: Bundle?) {
+        setSupportActionBar(vd.toolbar)
+        initNavigationView()
+    }
 
+    private fun initNavigationView(){
+        supportActionBar?.apply {
+            setDisplayShowTitleEnabled(true)
+            title = "W Mail"
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_navigation_view_button) // 뒤로가고 버튼 이미지 설정
+        }
+
+        vd.navigationView.setNavigationItemSelectedListener {
+//            it.isChecked = true
+            vd.drawerLayout.closeDrawers()
+            when(it.itemId){
+                R.id.menu_main_navigation_view_item_primary -> {
+                    showToast("click primary")
+                }
+                R.id.menu_main_navigation_view_item_social -> {
+                    showToast("click social")
+                }
+                R.id.menu_main_navigation_view_item_promotions -> {
+                    showToast("click promotions")
+                }
+            }
+            true
+        }
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            android.R.id.home -> {
+                vd.drawerLayout.openDrawer(GravityCompat.START)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun bind(savedInstanceState: Bundle?) {
