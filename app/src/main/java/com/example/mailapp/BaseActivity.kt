2b4b65994b3525e27a6dfe838197c458f49378a8
wjchoi96 <wjchoi96@naver.com>
2022-07-06 12:57:10 +0900

    [feat] base activity, base view model 추가

---
+++ app/src/main/java/com/example/mailapp/BaseActivity.kt
@@ -0,0 +1,54 @@
+package com.example.mailapp
+
+import android.content.Context
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<T: ViewDataBinding, R: BaseViewModel>: AppCompatActivity() {
+
+    abstract val vd: T
+    abstract val viewModel: R
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(vd.root)
+        initData(savedInstanceState)
+        initView(savedInstanceState)
+        bindAtBase()
+        bind(savedInstanceState)
+    }
+
+    abstract fun initData(savedInstanceState: Bundle?)
+    abstract fun initView(savedInstanceState: Bundle?)
+    abstract fun bind(savedInstanceState: Bundle?)
+
+    private fun bindAtBase(){
+        viewModel.showToastEvent.observe(this){
+            showToast(it)
+        }
+    }
+
+    protected fun setKeyboardShown(shown: Boolean, view: View){
+        val imm = (getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager)
+        if(shown) // flag 0은 추가동작이 없음을 알림
+            imm.showSoftInput(view, 0)
+        else
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    protected fun setInputAdjustMode(adjustResize: Boolean){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(adjustResize.not()) // false is SOFT_INPUT_ADJUST_RESIZE
+        } else {
+            if(adjustResize)
+                window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
+            else
+                window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/mailapp/BaseViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.mailapp
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+abstract class BaseViewModel: ViewModel() {
+
+    /**
+     * event
+     */
+    private val _showToastEvent: MutableLiveData<String> = MutableLiveData()
+    val showToastEvent: LiveData<String> = _showToastEvent
+    protected fun showToast(message: String) = _showToastEvent.postValue(message)
+}
